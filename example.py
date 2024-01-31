ac18856c74aa59494f342321ec3cb5ba916dc9c2
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-31 14:29:31 -0300

    Aula 01

--- example.py
+++
@@ -1,6 +0,0 @@
-def my_func():
-    print('Olá')
-
-# Pylint
-# snake_case -> Funções, Variáveis, Metódos
-# PascalCase -> Classes
---
+++ run_raw.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from barcode import Code128
+from barcode.writer import ImageWriter
+
+app = Flask(__name__)
+
+@app.route('/create_tag', methods=["POST"])
+def create_tag():
+    body = request.json
+    product_code = body.get('product_code')
+
+    tag = Code128(product_code, writer=ImageWriter())
+    path_from_tag = f'{tag}'
+    tag.save(path_from_tag)
+
+    return jsonify({ "tag_path": path_from_tag })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
